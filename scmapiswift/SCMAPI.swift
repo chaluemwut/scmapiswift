19156d36c9f2154aad634ca6ae445769598ec933
chaluemwut <chaluemwut@gmail.com>
2015-08-30 01:10:48 +0700

    test fix call

--- scmapiswift/SCMAPI.swift
+++ scmapiswift/SCMAPI.swift
@@ -10,14 +10,21 @@ import Foundation
 
 public class ResponseStatus {
     public var credValue:Double = 0.5
-    public var shoudbeMeasurement:Bool = false        
+    public var shoudbeMeasurement:Bool = false
+}
+
+public class RequestParameter {
+    public var socialMediaType:String = ""
+    public var message = ""
+    public var twitterId:String = ""
+    public var facebookLike:Double = 0
 }
 
 public class SCMAPI {
     
     public init(){}
     
-    public func processCredStandard() -> ResponseStatus {
+    public func processCredStandard(var reqParam:RequestParameter) -> ResponseStatus {
         let ret = ResponseStatus()
         ret.credValue = 0.5
         ret.shoudbeMeasurement = true
